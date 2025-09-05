5b23203bb070e74d91e3fcd274d0cedd17587d53
Shiroe <tarek.taleb85@gmail.com>
2025-09-05 20:25:10 +0200

    Fix project name in package-lock.json and update app.component.html with new links and comments; adjust angular.json for CLI analytics setting.

---
+++ FFxLH/src/pages/guidesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GuidesPage() {
+    return (
+        <div>
+            <h1>Bienvenue sur la page d'accueil</h1>
+        </div>
+    );
+}
+
+export default GuidesPage;
